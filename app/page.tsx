c275e6dd8396a3a936038e87ada16fffde01d3da
carayee <carayee@students.cs.ubc.cacara@MacBook-Air ~ % git config --global user.email carayee@students.cs.ubc.ca>
2026-02-15 11:55:41 -0700

    Update to experiences

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Slideshow from "./components/Slideshow";
 
 export default function Home() {
   return (
-    <div className="h-screen overflow-hidden bg-[#f2f0e8] font-sans dark:bg-black">
+    <div className="h-screen overflow-hidden bg-[#f9f8f5] font-sans dark:bg-black">
       <div className="flex h-full w-full">
         {/* ================= LEFT PANEL ================= */}
         <section className="w-1/2 overflow-y-auto no-scrollbar">
-          <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-[#f2f0e8] dark:bg-black text-center">
+          <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-[#f9f8f5] dark:bg-black text-center">
             {/* LEFT CONTENT */}
             <h1 className="text-7xl font-semibold text-black dark:text-zinc-50">
               CARA YEE
@@ -66,12 +66,73 @@ export default function Home() {
               </a>
             </p>
 
+            <h1 className="text-4xl font-semibold text-black dark:text-zinc-50">
+              2026
+              <br />
+              <br />
+            </h1>
+
+            <div className="flex w-full text-lg text-zinc-700 dark:text-zinc-400">
+              <a
+                href="https://www.shell.ca/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className=" flex items-center gap-4 bg-gray-200 px-6 py-4 rounded-xl
+                transition-all duration-300 hover:bg-gray-300 hover:shadow-md"
+              >
+                {/* Logo */}
+                <img
+                  src="/Shell.jpg"
+                  alt="Shell logo"
+                  className="w-8 h-8 rounded"
+                />
+
+                {/* Text */}
+                <div className="flex items-center gap-6">
+                  <span className="font-semibold text-lg">Shell</span>
+                  <span className="text-lg">
+                    Technical Product Manager | Internship
+                  </span>
+                </div>
+              </a>
+            </div>
+
+            <h1 className="text-4xl font-semibold text-black dark:text-zinc-50">
+              2025
+              <br />
+              <br />
+            </h1>
+
+            <div className="flex w-full text-lg text-zinc-700 dark:text-zinc-400">
+              <a
+                href="https://www.connacheroil.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className=" flex items-center gap-4 bg-gray-200 px-6 py-4 rounded-xl
+                transition-all duration-300 hover:bg-gray-300 hover:shadow-md"
+              >
+                {/* Logo */}
+                <img
+                  src="/Connacher.jpg"
+                  alt="Connacher logo"
+                  className="w-8 h-8 rounded"
+                />
+
+                {/* Text */}
+                <div className="flex items-center gap-6">
+                  <span className="font-semibold text-lg">Connacher</span>
+                  <span className="text-lg">
+                    Marketing Data Analyst | Internship
+                  </span>
+                </div>
+              </a>
+            </div>
+
             {/* Scroll filler
             {Array.from({ length: 20 }).map((_, i) => (
               <p key={i}>Right panel content {i + 1}</p>
             ))}
             */}
-
           </main>
         </section>
       </div>
